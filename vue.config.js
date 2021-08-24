b1b4b915bb21e364bdfceba23e1a25ad2ba2571a
katymykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-08-24 13:50:26 +0300

    -

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/filmoteka-vue/' : '/',
-  // publicPath: "/filmoteka-vue/",
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ? '/filmoteka-vue/' : '/',
+//   // publicPath: '/filmoteka-vue/',
+// };
