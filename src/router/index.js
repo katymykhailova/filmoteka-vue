17bc46e1b02524b26a275227bf91ef013f998616
katymykhailova <77439323+katymykhailova@users.noreply.github.com>
2021-08-28 10:25:55 +0300

    add Lazy Loading Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import { createWebHistory, createRouter } from 'vue-router';
-import Home from '../views/Home.vue';
-import Movies from '../views/Movies.vue';
-import Movie from '../views/Movie.vue';
-import Cast from '../views/Cast.vue';
-import Reviews from '../views/Reviews.vue';
-import Library from '../views/Library.vue';
+
+// import Home from '../views/Home.vue';
+// import Movies from '../views/Movies.vue';
+// import Movie from '../views/Movie.vue';
+// import Cast from '../views/Cast.vue';
+// import Reviews from '../views/Reviews.vue';
+// import Library from '../views/Library.vue';
+const Home = () => import(/* webpackChunkName: "Home" */ '../views/Home.vue');
+const Movies = () =>
+  import(/* webpackChunkName: "Movies" */ '../views/Movies.vue');
+const Movie = () =>
+  import(/* webpackChunkName: "Movie" */ '../views/Movie.vue');
+const Cast = () => import(/* webpackChunkName: "Cast" */ '../views/Cast.vue');
+const Reviews = () =>
+  import(/* webpackChunkName: "Reviews" */ '../views/Reviews.vue');
+const Library = () =>
+  import(/* webpackChunkName: "Library" */ '../views/Library.vue');
 
 const routes = [
   {
